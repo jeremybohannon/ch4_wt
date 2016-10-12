5e79e8a147c5b4d062994d6f9d713021335f60b3
Jeremy Bohannon <jeremybohannon@outlook.com>
2016-10-12 19:43:54 +0000

    Done with Section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,15 +10,30 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.html.haml by default
   end
   
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
+  
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
+  def update
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
   private
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
-  def new
-    @movie = Movie.new
-    # default: render 'new' template
-  end 
-
 end
